566ba98d094382133d8724344a25e355a5d2a51f
Arham Chowdhry <arham.chowdhry@abbott.com>
2024-05-03 20:03:40 +0530

    dynamic routes and storing state in URL

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,58 @@
 import { BrowserRouter, Route, Routes } from "react-router-dom";
+import { useEffect, useReducer, useState } from "react";
+
 import Product from "./pages/Product";
 import Homepage from "./pages/Homepage";
 import Pricing from "./pages/Pricing";
 import AppLayout from "./pages/AppLayout";
 import Login from "./pages/Login";
 import PageNotFound from "./pages/PageNotFound";
+import CityList from "./components/CityList";
+import CountryList from "./components/CountryList";
+import City from "./components/City";
+
+const BASE_URL = "http://localhost:8000";
 
 function App() {
+  const [cities, setCities] = useState([]);
+  const [isLoading, setIsLoading] = useState(false);
+
+  useEffect(function () {
+    async function fetchCities() {
+      try {
+        setIsLoading(true);
+        let res = await fetch(`${BASE_URL}/cities`);
+        let data = await res.json();
+        setCities(data);
+      } catch {
+        throw new Error("Something went wrong");
+      } finally {
+        setIsLoading(false);
+      }
+    }
+    fetchCities();
+  }, []);
   return (
     <BrowserRouter>
       <Routes>
         <Route path="/" element={<Homepage />}></Route>
         <Route path="product" element={<Product />}></Route>
         <Route path="pricing" element={<Pricing />}></Route>
-        <Route path="/app" element={<AppLayout />}></Route>
+        <Route path="/app" element={<AppLayout />}>
+          <Route
+            index
+            element={<CityList cities={cities} isLoading={isLoading} />}
+          ></Route>
+          <Route
+            path="cities"
+            element={<CityList cities={cities} isLoading={isLoading} />}
+          ></Route>
+          <Route path="cities/:id" element={<City />} />
+          <Route
+            path="countries"
+            element={<CountryList cities={cities} isLoading={isLoading} />}
+          ></Route>
+        </Route>
         <Route path="/login" element={<Login />} />
         <Route path="*" element={<PageNotFound />}></Route>
       </Routes>
